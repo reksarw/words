e3d11404a1805a11c3db0cc670d0fa06057cd40b
reksarw <reksarw@gmail.com>
2018-04-01 13:14:36 +0700

    Random Endpoint

--- src/modules/Jokes.php
+++ src/modules/Jokes.php
@@ -8,4 +8,8 @@ $app->group('/jokes', function() use ($app){
 	$app->get('/', function(Request $request, Response $response) {
 		return $response->withJson(['is_ok' => 'Yep']);
 	});
+
+	$app->get('/random', function(Request $request, Response $response){
+		return $response->withJson(['is_ok' => 'Yep']);
+	});
 });
