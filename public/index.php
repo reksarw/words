61629d542b7c1a05a7c33095982049aaeeb99181
reksarw <reksarw@gmail.com>
2018-04-06 10:12:01 +0700

    Delete helpers

--- public/index.php
+++ public/index.php
@@ -17,9 +17,6 @@ session_start();
 $settings = require __DIR__ . '/../src/settings.php';
 $app = new \Slim\App($settings);
 
-// Set up helper
-require __DIR__ . '/../src/helpers.php';
-
 // Set up dependencies
 require __DIR__ . '/../src/dependencies.php';
 
